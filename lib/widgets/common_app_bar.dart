1a1b262fac0beb8952b04369dc2169ee93774d3c
Patrick <24683548+pohuing@users.noreply.github.com>
2022-05-18 13:56:19 +0200

    Add overscroll to title

--- lib/widgets/common_app_bar.dart
+++ lib/widgets/common_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../blocs/authentication_bloc.dart';
@@ -7,40 +8,50 @@ import '../blocs/theme_cubit.dart';
 class CommonAppBar extends AppBar {
   final String _title;
 
-
-  CommonAppBar({Key? key, required String title}) : _title = title, super(key: key, actions: [
-    PopupMenuButton(
-      itemBuilder: (context) {
-        return [
-          PopupMenuItem(
-            onTap: () => context.read<ThemeCubit>().switchTheme(),
-            child: Row(
-              children: [const
-                Text('Toggle Theme'),
-                const Spacer(),
-                BlocBuilder<ThemeCubit, ThemeState>(
-                  builder: (context, state) => state.isBright
-                      ? const Icon(Icons.brightness_2_outlined)
-                      : const Icon(Icons.brightness_1_outlined),
-                )
-              ],
-            ),
-          ),
-          if (context.read<AuthenticationCubit>().state is SignedInState)
-            PopupMenuItem(
-              onTap: () async {
-                await context.read<AuthenticationCubit>().signOut();
-                await Navigator.of(context, rootNavigator: true).pushReplacementNamed('/');
-              },
-              child: Row(
-                children: const [Text('Sign out'), Spacer(), Icon(Icons.logout)],
-              ),
-            ),
-        ];
-      },
-    )
-  ]);
+  CommonAppBar({Key? key, required String title})
+      : _title = title,
+        super(key: key, actions: [
+          PopupMenuButton(
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(
+                  onTap: () => context.read<ThemeCubit>().switchTheme(),
+                  child: Row(
+                    children: [
+                      const Text('Toggle Theme'),
+                      const Spacer(),
+                      BlocBuilder<ThemeCubit, ThemeState>(
+                        builder: (context, state) => state.isBright
+                            ? const Icon(Icons.brightness_2_outlined)
+                            : const Icon(Icons.brightness_1_outlined),
+                      )
+                    ],
+                  ),
+                ),
+                if (context.read<AuthenticationCubit>().state is SignedInState)
+                  PopupMenuItem(
+                    onTap: () async {
+                      await context.read<AuthenticationCubit>().signOut();
+                      await Navigator.of(context, rootNavigator: true)
+                          .pushReplacementNamed('/');
+                    },
+                    child: Row(
+                      children: const [
+                        Text('Sign out'),
+                        Spacer(),
+                        Icon(Icons.logout)
+                      ],
+                    ),
+                  ),
+              ];
+            },
+          )
+        ]);
 
   @override
-  Widget get title => Text(_title);
+  Widget get title => SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        reverse: true,
+        child: Text(_title),
+      );
 }
